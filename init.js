2c9c3919eb8a5251bd085f05b9f596dd5e9dbd01
Mathias Kofoed <mathikofoed@gmail.com>
2017-08-04 10:10:29 +0200

    v0.1

---
+++ init.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {urlMatches: 'https:\/\/www.twitch.tv\/.+' },
+          })
+        ],
+        actions: [ 
+            new chrome.declarativeContent.ShowPageAction()
+        ]
+      }
+    ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(function(tab){
+    let channel = tab.url.split('/').pop();
+    chrome.tabs.update(tab.id, {url: "https://nightdev.com/twitchcast/?c=" + channel});
+});
+
---
+++ setchannel.js
@@ -0,0 +1,2 @@
+document.getElementById('cast-channel').value = window.location.href.split('?c=').pop();
+document.querySelector(".btn-cast").click();
